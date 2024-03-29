09a98d5fa38e1a804638d79140eee40f9e8c9303
Raquelle M. Campos <raquellecampos12@gmail.com>
2024-03-29 15:35:15 -0400

    Update variaveis_constantes_complexidade_1.py

--- secao_3/variaveis_constantes_complexidade_1.py
+++ secao_3/variaveis_constantes_complexidade_1.py
@@ -5,7 +5,7 @@ RADAR_RANGE = 1  # A distância onde o radar pega
 velocidade = 100
 local_carro = 100
 
-vel_carro_pass_radar_1 = velocidade > RADAR_1
+vel_carro_pass_radar_1 = velocidade > RADAR_1  #velocidade  maior que o radar
 carro_passou_radar_1 = local_carro >= (LOCAL_1 - RADAR_RANGE) and \
     local_carro <= (LOCAL_1 + RADAR_RANGE)
 carro_multado_radar_1 = carro_passou_radar_1 and vel_carro_pass_radar_1
